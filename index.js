9eb2ca0f8baf3a3dd53e0e2a3d1e72039a6f36e3
Kairat619 <105009235+Kairat619@users.noreply.github.com>
2024-04-19 11:34:58 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 'use strict';
 const OPT = {
     "user" : "admin", //后台密码
-    "password" : "yourdog", //后台密码
-    "siteDomain" : "blog.bilimlink.kz",// 域名(不带https 也不带/)
+    "password" : "yourSelfPwd", //后台密码
+    "siteDomain" : "blog.gezhong.vip",// 域名(不带https 也不带/)
     "siteName" : "CF workers blog",//博客名称
     "siteDescription":"A Blog Powered By Cloudflare Workers and KV",//博客描述
     "keyWords":"cloudflare,KV,workers,blog",//关键字
-    "cacheZoneId":"eb92cc3a90956c4a4e42012aa7c8991e",//清理缓存用 cf区域 ID
-    "cacheToken":"cJozTIg4DMgp3l3j7CthZ0wi75VjP5btTy6bG-xb",//清理缓存用 cf API token
+    "cacheZoneId":"cc868e8edce4027ad4a735741111111",//清理缓存用 cf区域 ID
+    "cacheToken":"LNxRWH-MPMIGnp8qhyT8FUsjDRN6tdOnmaaaaaaa",//清理缓存用 cf API token
 	
     "pageSize" : 5,//每页文章数
     "recentlySize" : 6,//最近文章数
     "readMoreLength":150,//阅读更多截取长度	
     "cacheTime" : 60*60*24*0.5, //网页缓存时长(秒),建议=文章更新频率
-    "themeURL" : "https://github.com/Kairat619/cloudflare-workers-blog/master/themes/default2.0/", // 模板地址,以 "/"" 结尾
+    "themeURL" : "https://raw.githubusercontent.com/gdtool/cloudflare-workers-blog/master/themes/default2.0/", // 模板地址,以 "/"" 结尾
     "html404" : `<b>404</b>`,//404页面代码
     "codeBeforHead":``,//其他代码,显示在</head>前
     "codeBeforBody":``,//其他代码,显示在</body>前
